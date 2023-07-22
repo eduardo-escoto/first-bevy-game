1783e76d7ed269206946946993588d1e17b640be
Eduardo Escoto <eduardo.escoto@noom.com>
2023-07-22 16:19:02 -0700

    Used resources to limit console output rate

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,16 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
-fn hello_world() {
-    println!("hello world! this is my first game :)")
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -27,8 +29,9 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, greet_people));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
     }
 }
 
